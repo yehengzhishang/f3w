5c1fcb7a2cb06e2a7bd2aafb63af94bf4cf2fdf2
zhaozhe <zhaozhe@xiaozhu.com>
2019-10-21 22:34:11 +0800

    theme no action bar

--- app/src/main/java/com/yu/zz/App.kt
+++ app/src/main/java/com/yu/zz/App.kt
@@ -1,5 +1,12 @@
 package com.yu.zz
 
 import android.app.Application
+import com.yu.zz.debug.DebugManager
+import com.yu.zz.fwww.R
 
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DebugManager.INSTANCE.themeId = R.style.AppTheme_NoActionBar
+    }
+}
