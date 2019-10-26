e5bf821450517d219eabc210f51ecb2a75df0eb3
zhaozhe <zhaozhe@xiaozhu.com>
2019-10-26 15:42:14 +0800

    add init in app

--- app/src/main/java/com/yu/zz/App.kt
+++ app/src/main/java/com/yu/zz/App.kt
@@ -7,6 +7,7 @@ import com.yu.zz.fwww.R
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        DebugManager.INSTANCE.init(this)
         DebugManager.INSTANCE.themeId = R.style.AppTheme_NoActionBar
     }
 }
