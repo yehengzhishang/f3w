4667f37ea580a0be8f6c44843db6209d8b8d1482
zhaozhe <yehengzhishang@163.com>
2019-10-27 22:13:51 +0800

    add

--- buildSrc/src/main/groovy/com.yu.zz.plugin/DemoPlugin.groovy
+++ buildSrc/src/main/groovy/com.yu.zz.plugin/DemoPlugin.groovy
@@ -4,19 +4,45 @@ import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin
 import org.jetbrains.kotlin.gradle.plugin.KotlinAndroidPluginWrapper
 
 class DemoPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         println("apply start ")
+        bean(project)
+
+        println("apply end")
+    }
+
+    static ProjectBean bean(Project project) {
         ProjectBean bean = new ProjectBean()
         bean.isApp = project.plugins.hasPlugin(AppPlugin)
         bean.isLibrary = project.plugins.hasPlugin(LibraryPlugin)
         bean.isKotlin = project.plugins.hasPlugin(KotlinAndroidPluginWrapper)
+        bean.isKapt = project.plugins.hasPlugin(Kapt3GradleSubplugin)
         println(bean.toString())
-        println("apply end")
+        return bean
     }
+
+    static String getApiApt(boolean isAndroid, boolean isKotlin) {
+        if (!isAndroid) {
+            throw new RuntimeException("not android project ")
+        }
+
+        if (isKotlin) {
+
+        }
+        return "annotationProcessor"
+    }
+}
+
+class ProjectUtils {
+    private ProjectUtils() {
+
+    }
+
 }
 
 class ProjectBean {
@@ -26,6 +52,13 @@ class ProjectBean {
     boolean isLibrary
     // 是否支持 kotlin
     boolean isKotlin
+
+    // 是否 已支持apt
+    boolean isKapt
+
+    boolean isAndroid() {
+        return isApp || isLibrary
+    }
 
     @Override
     String toString() {
