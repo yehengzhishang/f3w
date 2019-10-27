4dabfff9782a288aa717c680e9deac93084c6931
zhaozhe <yehengzhishang@163..com>
2019-10-27 08:36:05 +0800

    android module

--- buildSrc/src/main/groovy/com.yu.zz.plugin/DemoPlugin.groovy
+++ buildSrc/src/main/groovy/com.yu.zz.plugin/DemoPlugin.groovy
@@ -1,5 +1,6 @@
 package com.yu.zz.plugin
 
+import com.android.build.gradle.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,7 +8,26 @@ class DemoPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         println("apply start ")
-
+        ProjectBean bean = new ProjectBean()
+        bean.isApp = project.plugins.hasPlugin(AppPlugin)
+        bean.isLibrary = project.plugins.hasPlugin(LibraryPlugin)
+        
+        println(bean.toString())
         println("apply end")
+    }
+}
+
+class ProjectBean {
+    // 是否 Application
+    boolean isApp
+    // 是否 为android Library
+    boolean isLibrary
+
+    @Override
+    String toString() {
+        return "ProjectBean{" +
+                "isApp=" + isApp +
+                ", isLibrary=" + isLibrary +
+                '}'
     }
 }
