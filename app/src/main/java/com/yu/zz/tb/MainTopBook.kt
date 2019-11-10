2239cab6499accb6318cc310ba54a6f320870b2f
zhaozhe <yehengzhishang@163.com>
2019-11-10 08:54:38 +0800

    class : top book class viewmodel services bean

--- app/src/main/java/com/yu/zz/tb/MainTopBook.kt
+++ app/src/main/java/com/yu/zz/tb/MainTopBook.kt
@@ -1,7 +1,27 @@
 package com.yu.zz.tb
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.http.Query
 
 class MainTopBookActivity : AppCompatActivity() {
 
 }
+
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+    private val mDataTechnique: MutableLiveData<TechniqueBean> by lazy { MutableLiveData<TechniqueBean>() }
+
+    fun getDataTechnique(): LiveData<TechniqueBean> = mDataTechnique
+
+    fun getTechnique(start: Int, limit: Int) {
+    }
+}
+
+interface TopBookServices {
+    fun getTechnique(@Query("start") start: String, @Query("limit") limit: Int)
+}
+
+class TechniqueBean
