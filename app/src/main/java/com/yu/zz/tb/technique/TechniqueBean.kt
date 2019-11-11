b85ecb1028639e649621ccc6fbb73404518114f8
zhaozhe <yehengzhishang@163.com>
2019-11-11 22:56:05 +0800

    complete bean

--- app/src/main/java/com/yu/zz/tb/technique/TechniqueBean.kt
+++ app/src/main/java/com/yu/zz/tb/technique/TechniqueBean.kt
@@ -1,3 +1,22 @@
 package com.yu.zz.tb.technique
 
-class TechniqueBean
+import com.google.gson.annotations.SerializedName
+
+class TechniqueBean {
+    @SerializedName("articleId")
+    var articleId: Int? = null
+    @SerializedName("categoryId")
+    var categoryId: Int? = null
+    @SerializedName("title")
+    var title: String? = null
+    @SerializedName("description")
+    var description: String? = null
+    @SerializedName("cover")
+    var cover: String? = null
+    @SerializedName("createTime")
+    var createTime: String? = null
+    @SerializedName("like")
+    var like: Boolean? = null
+    @SerializedName("likeTotal")
+    var likeTotal: Int? = null
+}
