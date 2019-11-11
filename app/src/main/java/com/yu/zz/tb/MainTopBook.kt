60756b1092aca6d7bfa7ed78889022570dd252d2
zhaozhe <yehengzhishang@163.com>
2019-11-11 23:36:47 +0800

    add request data

--- app/src/main/java/com/yu/zz/tb/MainTopBook.kt
+++ app/src/main/java/com/yu/zz/tb/MainTopBook.kt
@@ -1,22 +1,60 @@
 package com.yu.zz.tb
 
 import android.app.Application
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.yu.zz.tb.technique.TechniqueBean
+import androidx.lifecycle.Observer as OB
+import androidx.lifecycle.ViewModelProviders
+import com.google.gson.GsonBuilder
+import com.yu.zz.tb.deep.TopBookApi
+import com.yu.zz.tb.deep.TopBookBean
+import com.yu.zz.tb.technique.DataBean
+import com.yu.zz.tb.technique.TechniqueService
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainTopBookActivity : AppCompatActivity() {
-
+    private val mViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewModel.getDataTechnique().observe(this, OB {
+            if (it == null) {
+                return@OB
+            }
+            val content = GsonBuilder().setPrettyPrinting().create().toJson(it)
+        })
+        mViewModel.getTechnique()
+    }
 }
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
-    private val mDataTechnique: MutableLiveData<TechniqueBean> by lazy { MutableLiveData<TechniqueBean>() }
+    private val mDataTechnique: MutableLiveData<TopBookBean<DataBean>> by lazy { MutableLiveData<TopBookBean<DataBean>>() }
 
-    fun getDataTechnique(): LiveData<TechniqueBean> = mDataTechnique
+    fun getDataTechnique(): LiveData<TopBookBean<DataBean>> = mDataTechnique
 
-    fun getTechnique(start: Int, limit: Int) {
+    fun getTechnique(start: Int = 0, limit: Int = 8) {
+        TopBookApi.INSTANCE.retrofit.create(TechniqueService::class.java)
+                .getTechnique(start.toString(), limit.toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<TopBookBean<DataBean>> {
+                    override fun onComplete() {
+                    }
 
+                    override fun onSubscribe(d: Disposable) {
+                    }
+
+                    override fun onNext(t: TopBookBean<DataBean>) {
+                        mDataTechnique.value = t
+                    }
+
+                    override fun onError(e: Throwable) {
+                    }
+                })
     }
 }
