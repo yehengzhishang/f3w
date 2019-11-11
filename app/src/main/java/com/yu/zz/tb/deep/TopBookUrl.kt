6375396766979670ef54f79b75dfb9d4999e8d59
zhaozhe <yehengzhishang@163.com>
2019-11-11 23:02:42 +0800

    fix code

--- app/src/main/java/com/yu/zz/tb/deep/TopBookUrl.kt
+++ app/src/main/java/com/yu/zz/tb/deep/TopBookUrl.kt
@@ -6,5 +6,3 @@ const val TOPBOOK_URL_TECHNIQUE = "webapi/content/article/9/page"
 
 //const val TOPBOOK_URL_PATH_PREFIX = "webapi/content/article/%s/page"
 //fun getTopBookUrlPathString(pos: String): String = String.format(TOPBOOK_URL_PATH_PREFIX, pos)
-
-
