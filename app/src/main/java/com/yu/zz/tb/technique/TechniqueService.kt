0533be5a006019f30279d3cd7051c84618d86250
zhaozhe <yehengzhishang@163.com>
2019-11-11 23:03:07 +0800

    update code

--- app/src/main/java/com/yu/zz/tb/technique/TechniqueService.kt
+++ app/src/main/java/com/yu/zz/tb/technique/TechniqueService.kt
@@ -1,11 +1,12 @@
 package com.yu.zz.tb.technique
 
 import com.yu.zz.tb.deep.TOPBOOK_URL_TECHNIQUE
+import com.yu.zz.tb.deep.TopBookBean
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TopBookService {
     @GET(TOPBOOK_URL_TECHNIQUE)
-    fun getTechnique(@Query("start") start: String, @Query("limit") limit: Int): Observable<TechniqueBean>
+    fun getTechnique(@Query("start") start: String, @Query("limit") limit: Int): Observable<TopBookBean<DataBean>>
 }
