b1daacca3ecd429228dc37b548b0caee81b4994d
zhaozhe <yehengzhishang@163.com>
2019-11-12 21:04:40 +0800

    delte old

--- app/src/main/java/com/yu/zz/tb/deep/TopBookUrl.kt
+++ app/src/main/java/com/yu/zz/tb/deep/TopBookUrl.kt
@@ -4,7 +4,5 @@ const val TOPBOOK_URL_BASE = "https://topbook.cc/"
 
 const val TOPBOOK_INDEX_TECHNIQUE = 9
 
-const val TOPBOOK_URL_TECHNIQUE = "webapi/content/article/${TOPBOOK_INDEX_TECHNIQUE}/page"
-
 const val TOPBOOK_URL_PATH_PREFIX = "webapi/content/article/{index}/page"
 //fun getTopBookUrlPathString(pos: String): String = String.format(TOPBOOK_URL_PATH_PREFIX, pos)
