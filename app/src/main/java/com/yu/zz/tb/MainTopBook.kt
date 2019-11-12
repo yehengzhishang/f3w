03e03813ed05338352f2e4c5c2a17a92e68f986e
zhaozhe <yehengzhishang@163.com>
2019-11-13 00:09:05 +0800

    add rx

--- app/src/main/java/com/yu/zz/tb/MainTopBook.kt
+++ app/src/main/java/com/yu/zz/tb/MainTopBook.kt
@@ -2,14 +2,13 @@ package com.yu.zz.tb
 
 import android.app.Application
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModelProviders
-import com.google.gson.GsonBuilder
 import com.yu.zz.fwww.R
 import com.yu.zz.tb.arrange.goToThreadMain
 import com.yu.zz.tb.deep.*
+import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 
@@ -26,6 +25,7 @@ class MainTopBookActivity : AppCompatActivity() {
 }
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
+    private val mMapCategory = HashMap<String, TopBookPageBean>()
     fun getPage(start: Int = 0, limit: Int = 20) {
         TopBookApi.INSTANCE.retrofit.create(TopBookService::class.java)
                 .getPageConfig(start.toString(), limit.toString())
@@ -47,32 +47,16 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     }
 
     private fun getItems(bean: TopBookPageResponseBean) {
-        if (!bean.isSuccess()) {
-            return
-        }
-        if (bean.data == null) {
-            return
-        }
-        val listBean: TopBookListBean<TopBookPageBean> = bean.data!!
-        if (listBean.items == null) {
-            return
-        }
-        val list: MutableList<TopBookPageBean?> = listBean.items!!
-        for (itemBean in list) {
-            if (itemBean == null) {
-                continue
-            }
-            if (itemBean.categoryId == null) {
-                return
-            }
-            val itemId: String = itemBean.categoryId!!.toString()
-            getItem(itemId)
-        }
-    }
-
-    private fun getItem(itemId: String, start: Int = 0, limit: Int = 8) {
-        TopBookApi.INSTANCE.retrofit.create(TopBookService::class.java)
-                .getTopBookList(itemId, start.toString(), limit.toString())
+        Observable.just(bean)
+                .filter { it.isSuccess() && it.data != null }
+                .map { it.data!! }
+                .filter { it.items != null }
+                .map { it.items!! }
+                .flatMap { Observable.fromIterable(it) }
+                .filter { it.categoryId != null }
+                .doOnNext { mMapCategory[it!!.categoryId!!.toString()] = it }
+                .map { it.categoryId!! }
+                .flatMap { getObsItem(it.toString()) }
                 .goToThreadMain()
                 .subscribe(object : Observer<TopBookResponseBean> {
                     override fun onComplete() {
@@ -82,13 +66,33 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
                     }
 
                     override fun onNext(t: TopBookResponseBean) {
-                        val result = GsonBuilder().setPrettyPrinting().create().toJson(t)
-                        Log.e("Rain", result)
+                        val itemId = itemId(t) ?: return
+                        if (!mMapCategory.keys.contains(itemId)) {
+                            return
+                        }
+                        
                     }
 
                     override fun onError(e: Throwable) {
-                        Log.e("rain", "error")
                     }
                 })
+    }
+
+    private fun itemId(t: TopBookResponseBean): String? {
+        return if (t.isSuccess() && t.data != null && t.data!!.items != null
+                && t.data!!.items!!.isNotEmpty()) {
+            t.data!!.items!![0]?.toString()
+        } else {
+            null
+        }
+    }
+
+    private fun getObsItem(itemId: String, start: Int = 0, limit: Int = 8): Observable<TopBookResponseBean> {
+        return TopBookApi.INSTANCE.retrofit.create(TopBookService::class.java)
+                .getTopBookList(itemId, start.toString(), limit.toString())
+    }
+
+    private fun addItem(itemId: String, bean: TopBookResponseBean) {
+
     }
 }
