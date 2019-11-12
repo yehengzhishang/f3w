34e56178d665574d8b740c8d45a76df68f643339
zhaozhe <yehengzhishang@163.com>
2019-11-12 21:01:23 +0800

    add topservice

---
+++ app/src/main/java/com/yu/zz/tb/deep/TopBookService.kt
@@ -0,0 +1,11 @@
+package com.yu.zz.tb.deep
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TopBookService {
+    @GET(TOPBOOK_URL_PATH_PREFIX)
+    fun getTopBookList(@Path("index") index: String, @Query("start") start: String, @Query("limit") limit: String): Observable<TopBookResponseBean>
+}
