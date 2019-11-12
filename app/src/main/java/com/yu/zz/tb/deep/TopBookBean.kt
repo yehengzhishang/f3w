1fbd8d0213b708c8ede885530aadabb92182f0e7
zhaozhe <yehengzhishang@163.com>
2019-11-12 21:01:52 +0800

    complete bean

--- app/src/main/java/com/yu/zz/tb/deep/TopBookBean.kt
+++ app/src/main/java/com/yu/zz/tb/deep/TopBookBean.kt
@@ -2,7 +2,7 @@ package com.yu.zz.tb.deep
 
 import com.google.gson.annotations.SerializedName
 
-class TopBookBean<T> {
+open class TopBookBean<T> {
     @SerializedName("success")
     var success: Boolean? = null
     @SerializedName("data")
@@ -10,3 +10,35 @@ class TopBookBean<T> {
 
     fun isSuccess(): Boolean = success ?: false
 }
+
+class TopBookListBean<T> {
+    @SerializedName("start")
+    var start: Int? = null
+    @SerializedName("limit")
+    var limit: Int? = null
+    @SerializedName("total")
+    var total: Int? = null
+    @SerializedName("items")
+    var items: MutableList<T?>? = null
+}
+
+class TopBookDataBean {
+    @SerializedName("articleId")
+    var articleId: Int? = null
+    @SerializedName("categoryId")
+    var categoryId: Int? = null
+    @SerializedName("title")
+    var title: String? = null
+    @SerializedName("description")
+    var description: String? = null
+    @SerializedName("cover")
+    var cover: String? = null
+    @SerializedName("createTime")
+    var createTime: String? = null
+    @SerializedName("like")
+    var like: Boolean? = null
+    @SerializedName("likeTotal")
+    var likeTotal: Int? = null
+}
+
+class TopBookResponseBean : TopBookBean<TopBookListBean<TopBookDataBean>>()
