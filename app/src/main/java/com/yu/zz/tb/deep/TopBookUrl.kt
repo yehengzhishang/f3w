9d971395c0496a27f6c552c976edfd63c377531c
zhaozhe <yehengzhishang@163.com>
2019-11-12 21:02:46 +0800

    add index

--- app/src/main/java/com/yu/zz/tb/deep/TopBookUrl.kt
+++ app/src/main/java/com/yu/zz/tb/deep/TopBookUrl.kt
@@ -2,7 +2,9 @@ package com.yu.zz.tb.deep
 
 const val TOPBOOK_URL_BASE = "https://topbook.cc/"
 
-const val TOPBOOK_URL_TECHNIQUE = "webapi/content/article/9/page"
+const val TOPBOOK_INDEX_TECHNIQUE = 9
 
-//const val TOPBOOK_URL_PATH_PREFIX = "webapi/content/article/%s/page"
+const val TOPBOOK_URL_TECHNIQUE = "webapi/content/article/${TOPBOOK_INDEX_TECHNIQUE}/page"
+
+const val TOPBOOK_URL_PATH_PREFIX = "webapi/content/article/{index}/page"
 //fun getTopBookUrlPathString(pos: String): String = String.format(TOPBOOK_URL_PATH_PREFIX, pos)
