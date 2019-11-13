41be8cceb9238339fbb6317dd8b14f3b6244a918
zhaozhe <yehengzhishang@163.com>
2019-11-13 22:50:48 +0800

    add adpater

--- app/src/main/java/com/yu/zz/tb/MainTopBook.kt
+++ app/src/main/java/com/yu/zz/tb/MainTopBook.kt
@@ -3,23 +3,32 @@ package com.yu.zz.tb
 import android.app.Application
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
+import com.yu.zz.common.arrange.dp2px
+import com.yu.zz.common.arrange.getScreenWidth
 import com.yu.zz.fwww.R
 import com.yu.zz.tb.arrange.goToThreadMain
 import com.yu.zz.tb.deep.*
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_topbook_main.*
+import androidx.lifecycle.Observer as OB
 
 class MainTopBookActivity : AppCompatActivity() {
+    private val mAdapter = TopBookAdapter()
     private val mViewModel by lazy {
         ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
@@ -27,14 +36,28 @@ class MainTopBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topbook_main)
+        rv.adapter = mAdapter
+        rv.layoutManager = GridLayoutManager(this, 2)
+        mViewModel.getDataTp().observe(this, OB {
+            if (it == null) {
+                return@OB
+            }
+            mAdapter.add(it)
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
         mViewModel.getPage()
     }
 }
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     private val mMapCategory = HashMap<String, TopBookPageBean>()
+    private val mMapTp = HashMap<String, MutableList<TopBookDataBean>>()
     private val mListCategory = mutableListOf<TopBookPageBean>()
     private val mDataCategory by lazy { MutableLiveData<List<TopBookPageBean>>() }
+    private val mDataTp by lazy { MutableLiveData<List<TopBookDataBean>>() }
 
     fun getPage(start: Int = 0, limit: Int = 20) {
         TopBookApi.INSTANCE.retrofit.create(TopBookService::class.java)
@@ -58,6 +81,10 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
 
     fun getDataCategory(): LiveData<List<TopBookPageBean>> {
         return mDataCategory
+    }
+
+    fun getDataTp(): LiveData<List<TopBookDataBean>> {
+        return mDataTp
     }
 
     private fun getItems(bean: TopBookPageResponseBean) {
@@ -85,6 +112,14 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
                         if (!mMapCategory.keys.contains(itemId)) {
                             return
                         }
+                        var list = mMapTp.getOrPut(itemId, { mutableListOf() })
+                        val listSource = t.data?.items!!
+                        for (sourceBean in listSource) {
+                            if (sourceBean == null) {
+                                continue
+                            }
+                            list.add(sourceBean)
+                        }
                         Log.e("rain", GsonBuilder().setPrettyPrinting().create().toJson(t))
                     }
 
@@ -97,6 +132,13 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     private fun refreshData() {
         mListCategory.sortBy { it.categoryId }
         mDataCategory.postValue(mListCategory)
+        val listAll = mutableListOf<TopBookDataBean>()
+        for (category in mListCategory) {
+            val list: MutableList<TopBookDataBean>? = mMapTp[category.categoryId!!.toString()]
+                    ?: continue
+            listAll.addAll(list!!)
+        }
+        mDataTp.postValue(listAll)
     }
 
     private fun addCategory(bean: TopBookPageBean) {
@@ -120,16 +162,41 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     }
 }
 
-class TopBookAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val mListBean = mutableListOf<Any>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        
+class TopBookAdapter : RecyclerView.Adapter<TopBookViewHolder>() {
+    private val mListBean = mutableListOf<TopBookDataBean>()
+    fun add(list: List<TopBookDataBean>) {
+        mListBean.addAll(list)
+        notifyDataSetChanged()
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopBookViewHolder = TopBookViewHolder(parent)
 
     override fun getItemCount(): Int = mListBean.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: TopBookViewHolder, position: Int) {
+        holder.bind(mListBean[position])
     }
 }
 
-class TopBookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class TopBookViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+    private val ivPic = itemView.findViewById<ImageView>(R.id.ivPic)
+    private val tvTime = itemView.findViewById<TextView>(R.id.tvTime)
+    private val tvLike = itemView.findViewById<TextView>(R.id.tvLike)
+
+    constructor(parent: ViewGroup) : this(LayoutInflater.from(parent.context).inflate(R.layout.tp_item_tp, parent, false))
+
+    init {
+        val screenWidth = tvTitle.context.getScreenWidth()
+        val itemWidth = (screenWidth - tvTitle.context.dp2px(16) * 2 - tvTitle.context.dp2px(8)) / 2
+        val itemHeight = itemWidth * 3 / 4
+        ivPic.layoutParams.width = itemWidth
+        ivPic.layoutParams.height = itemHeight
+    }
+
+    fun bind(bean: TopBookDataBean) {
+        tvTitle.text = bean.title
+        tvTime.text = bean.createTime
+        tvLike.text = bean.likeTotal?.toString()
+    }
+}
