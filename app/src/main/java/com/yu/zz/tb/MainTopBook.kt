38fa0c4eb975d5ea0c3efc0b02d3b7c4691e77f1
zhaozhe <yehengzhishang@163.com>
2019-11-13 23:31:24 +0800

    add image

--- app/src/main/java/com/yu/zz/tb/MainTopBook.kt
+++ app/src/main/java/com/yu/zz/tb/MainTopBook.kt
@@ -1,12 +1,12 @@
 package com.yu.zz.tb
 
 import android.app.Application
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.AndroidViewModel
@@ -15,9 +15,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.gson.GsonBuilder
 import com.yu.zz.common.arrange.dp2px
-import com.yu.zz.common.arrange.getScreenWidth
 import com.yu.zz.fwww.R
 import com.yu.zz.tb.arrange.goToThreadMain
 import com.yu.zz.tb.deep.*
@@ -25,6 +25,7 @@ import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_topbook_main.*
+import pl.droidsonroids.gif.GifImageView
 import androidx.lifecycle.Observer as OB
 
 class MainTopBookActivity : AppCompatActivity() {
@@ -164,6 +165,24 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
 
 class TopBookAdapter : RecyclerView.Adapter<TopBookViewHolder>() {
     private val mListBean = mutableListOf<TopBookDataBean>()
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        recyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+                super.getItemOffsets(outRect, view, parent, state)
+                val position = parent.getChildLayoutPosition(view)
+                outRect.top = view.context.dp2px(8)
+                if (position % 2 == 0) {
+                    outRect.left = view.context.dp2px(16)
+                    outRect.right = view.context.dp2px(4)
+                } else {
+                    outRect.left = view.context.dp2px(4)
+                    outRect.right = view.context.dp2px(16)
+                }
+            }
+        })
+    }
+
     fun add(list: List<TopBookDataBean>) {
         mListBean.addAll(list)
         notifyDataSetChanged()
@@ -180,21 +199,14 @@ class TopBookAdapter : RecyclerView.Adapter<TopBookViewHolder>() {
 
 class TopBookViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
-    private val ivPic = itemView.findViewById<ImageView>(R.id.ivPic)
+    private val ivPic = itemView.findViewById<GifImageView>(R.id.ivPic)
     private val tvTime = itemView.findViewById<TextView>(R.id.tvTime)
     private val tvLike = itemView.findViewById<TextView>(R.id.tvLike)
 
     constructor(parent: ViewGroup) : this(LayoutInflater.from(parent.context).inflate(R.layout.tp_item_tp, parent, false))
 
-    init {
-        val screenWidth = tvTitle.context.getScreenWidth()
-        val itemWidth = (screenWidth - tvTitle.context.dp2px(16) * 2 - tvTitle.context.dp2px(8)) / 2
-        val itemHeight = itemWidth * 3 / 4
-        ivPic.layoutParams.width = itemWidth
-        ivPic.layoutParams.height = itemHeight
-    }
-
     fun bind(bean: TopBookDataBean) {
+        Glide.with(ivPic).load(bean.cover).into(ivPic)
         tvTitle.text = bean.title
         tvTime.text = bean.createTime
         tvLike.text = bean.likeTotal?.toString()
