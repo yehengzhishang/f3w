5396ba009b81b4fd7b7ab7cc39e229edb86259d8
zhaozhe <yehengzhishang@163.com>
2019-11-14 23:17:04 +0800

    add type

--- app/src/main/java/com/yu/zz/topbook/MainTopBook.kt
+++ app/src/main/java/com/yu/zz/topbook/MainTopBook.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -14,6 +13,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
@@ -38,8 +38,8 @@ class MainTopBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.topbook_activity_main)
-        rv.adapter = mAdapter
         rv.layoutManager = GridLayoutManager(this, 2)
+        rv.adapter = mAdapter
         mViewModel.getDataTp().observe(this, OB {
             if (it == null) {
                 return@OB
@@ -55,7 +55,7 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     private val mMapTp = HashMap<String, MutableList<ArticleTopBookBean>>()
     private val mListCategory = mutableListOf<CategoryTopBookBean>()
     private val mDataCategory by lazy { MutableLiveData<List<CategoryTopBookBean>>() }
-    private val mDataTp by lazy { MutableLiveData<List<ArticleTopBookBean>>() }
+    private val mDataTp by lazy { MutableLiveData<List<Any>>() }
 
     fun getPage(start: Int = 0, limit: Int = 20) {
         TopBookApi.INSTANCE.retrofit.create(TopBookService::class.java)
@@ -81,7 +81,7 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
         return mDataCategory
     }
 
-    fun getDataTp(): LiveData<List<ArticleTopBookBean>> {
+    fun getDataTp(): LiveData<List<Any>> {
         return mDataTp
     }
 
@@ -130,11 +130,12 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     private fun refreshData() {
         mListCategory.sortBy { it.categoryId }
         mDataCategory.postValue(mListCategory)
-        val listAll = mutableListOf<ArticleTopBookBean>()
+        val listAll = mutableListOf<Any>()
         for (category in mListCategory) {
-            val list: MutableList<ArticleTopBookBean>? = mMapTp[category.categoryId!!.toString()]
+            val list: MutableList<ArticleTopBookBean> = mMapTp[category.categoryId!!.toString()]
                     ?: continue
-            listAll.addAll(list!!)
+            listAll.add(category)
+            listAll.addAll(list)
         }
         mDataTp.postValue(listAll)
     }
@@ -156,10 +157,17 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     }
 }
 
+private const val VIEW_TYPE_A = 1
+private const val VIEW_TYPE_P = 2
+
 class TopBookAdapter : RecyclerView.Adapter<TopBookViewHolder<*>>() {
-    private val mListBean = mutableListOf<ArticleTopBookBean>()
+    private val mListBean = mutableListOf<Any>()
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         super.onAttachedToRecyclerView(recyclerView)
+        val layoutManager = recyclerView.layoutManager as GridLayoutManager
+        layoutManager.spanSizeLookup = object : SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int = getItemViewType(position)
+        }
         recyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
             override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
                 super.getItemOffsets(outRect, view, parent, state)
@@ -176,12 +184,18 @@ class TopBookAdapter : RecyclerView.Adapter<TopBookViewHolder<*>>() {
         })
     }
 
-    fun add(list: List<ArticleTopBookBean>) {
+    fun add(list: List<Any>) {
         mListBean.addAll(list)
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopBookViewHolder<*> = ArticleTopBookViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopBookViewHolder<*> {
+        return when (viewType) {
+            VIEW_TYPE_A -> ArticleTopBookViewHolder(parent)
+            VIEW_TYPE_P -> CategoryTopBookViewHolder(parent)
+            else -> throw RuntimeException("cant find view Type")
+        }
+    }
 
     override fun getItemCount(): Int = mListBean.size
 
@@ -190,6 +204,14 @@ class TopBookAdapter : RecyclerView.Adapter<TopBookViewHolder<*>>() {
             Snackbar.make(holder.itemView, "点击稍后", Snackbar.LENGTH_SHORT).show()
         }
         holder.bindAny(mListBean[position], position)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            mListBean[position] is ArticleTopBookBean -> VIEW_TYPE_A
+            mListBean[position] is CategoryTopBookBean -> VIEW_TYPE_P
+            else -> throw RuntimeException("cant find type")
+        }
     }
 }
 
@@ -213,14 +235,14 @@ class ArticleTopBookViewHolder private constructor(parent: ViewGroup, layoutId: 
     }
 }
 
-class TitleTopBookViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class CategoryTopBookViewHolder private constructor(parent: ViewGroup, layoutId: Int) : TopBookViewHolder<CategoryTopBookBean>(parent, layoutId) {
     private val tvTitleName = itemView.findViewById<TextView>(R.id.tvName)
     private val tvMore = itemView.findViewById<TextView>(R.id.btnMore)
 
-    constructor(parent: ViewGroup) : this(LayoutInflater.from(parent.context).inflate(R.layout.topbook_item_title, parent, false))
+    constructor(parent: ViewGroup) : this(parent, R.layout.topbook_item_title)
 
-    fun bind(categoryBean: CategoryTopBookBean) {
-        tvTitleName.text = categoryBean.name
+    override fun bind(bean: CategoryTopBookBean, position: Int) {
+        tvTitleName.text = bean.name
         tvMore.setOnClickListener {
             Snackbar.make(tvMore, "都闪开！我要跳转了！！！", Snackbar.LENGTH_SHORT).show()
         }
