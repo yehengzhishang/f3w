709c6dada895f6a5060c986e1068d475cdee79b4
zhaozhe <yehengzhishang@163.com>
2019-11-17 16:13:20 +0800

    add splash

---
+++ app/src/main/java/com/yu/zz/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.yu.zz
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yu.zz.fwww.R
+import com.yu.zz.topbook.MainTopBookActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        lav.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivity(Intent(this@SplashActivity, MainTopBookActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
+    }
+}
