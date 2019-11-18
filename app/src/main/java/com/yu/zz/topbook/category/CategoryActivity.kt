3d23a7988740a4a10498fc1f615fe9e35425a1a1
zhaozhe <yehengzhishang@163.com>
2019-11-18 23:05:07 +0800

    add key

--- app/src/main/java/com/yu/zz/topbook/category/CategoryActivity.kt
+++ app/src/main/java/com/yu/zz/topbook/category/CategoryActivity.kt
@@ -1,7 +1,12 @@
 package com.yu.zz.topbook.category
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class CategoryActivity : AppCompatActivity() {
+const val KEY_ID_CATEGORY = KEY_CATEGORY_ID
 
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
