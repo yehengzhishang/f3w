194b9916513f9122a9d06ba3b2cff277f64206d5
zhaozhe <zhaozhe@xiaozhu.com>
2019-11-24 19:07:14 +0800

    add life

--- app/src/main/java/com/yu/zz/App.kt
+++ app/src/main/java/com/yu/zz/App.kt
@@ -1,12 +1,14 @@
 package com.yu.zz
 
 import android.app.Application
+import com.yu.zz.biz.BizLifecycle
 import com.yu.zz.debug.DebugManager
 import com.yu.zz.fwww.R
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        this.registerActivityLifecycleCallbacks(BizLifecycle())
         DebugManager.INSTANCE.init(this)
         DebugManager.INSTANCE.themeId = R.style.AppTheme_NoActionBar
     }
