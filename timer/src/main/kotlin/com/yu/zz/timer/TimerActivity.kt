5e306a0006547eb54d17946a48f5f24ac32faeb3
zhaozhe <yehengzhishang@163.com>
2019-12-10 07:14:49 +0800

    add method time

--- timer/src/main/kotlin/com/yu/zz/timer/TimerActivity.kt
+++ timer/src/main/kotlin/com/yu/zz/timer/TimerActivity.kt
@@ -2,7 +2,11 @@ package com.yu.zz.timer
 
 import com.yu.zz.common.arrange.getScreenWidth
 import com.yu.zz.common.base.BaseActivity
+import io.reactivex.Flowable
+import io.reactivex.FlowableSubscriber
 import kotlinx.android.synthetic.main.activity_timer.*
+import org.reactivestreams.Subscription
+import java.util.concurrent.TimeUnit
 
 class TimerActivity : BaseActivity() {
     override fun layoutId(): Int = R.layout.activity_timer
@@ -10,9 +14,28 @@ class TimerActivity : BaseActivity() {
     override fun ui() {
         val lp = fl.layoutParams
         lp.height = getScreenWidth()
+        fl.setOnClickListener {
+
+        }
     }
 
     override fun data() {
-        
+    }
+
+    private fun timeInterval(info: TimeInfo) {
+        Flowable.interval(0, 1, TimeUnit.SECONDS)
+                .subscribe(object : FlowableSubscriber<Long> {
+                    override fun onComplete() {
+                    }
+
+                    override fun onSubscribe(s: Subscription) {
+                    }
+
+                    override fun onNext(t: Long?) {
+                    }
+
+                    override fun onError(t: Throwable?) {
+                    }
+                })
     }
 }
