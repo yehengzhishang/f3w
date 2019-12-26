78905927489e075b1e0cd645c83a6a5f7491f8a4
zhaozhe <yehengzhishang@163.com>
2019-12-26 22:31:07 +0800

    change new APi

--- app/src/main/java/com/yu/zz/topbook/deep/TopBookApi.kt
+++ app/src/main/java/com/yu/zz/topbook/deep/TopBookApi.kt
@@ -1,19 +1,14 @@
 package com.yu.zz.topbook.deep
 
-import okhttp3.OkHttpClient
+import com.yu.zz.common.net.FlyNet
+import com.yu.zz.common.net.FlyNetConfig
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class TopBookApi private constructor() {
-    private val clint = OkHttpClient.Builder()
-            .build()
-    val retrofit: Retrofit = Retrofit.Builder()
-            .client(clint)
-            .baseUrl(TOPBOOK_URL_BASE)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private val fly: FlyNet = FlyNet(FlyNetConfig(TOPBOOK_URL_BASE, RxJava2CallAdapterFactory.create(), GsonConverterFactory.create()))
+    val retrofit: Retrofit = fly.retrofit
 
     companion object {
         val INSTANCE = TopBookApi()
