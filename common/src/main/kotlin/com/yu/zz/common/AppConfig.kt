46d51c940f69a7f6180bb8bb4a850c10f09b97e6
zhaozhe <yehengzhishang@163.com>
2019-12-26 22:28:59 +0800

    app config

---
+++ common/src/main/kotlin/com/yu/zz/common/AppConfig.kt
@@ -0,0 +1,18 @@
+package com.yu.zz.common
+
+private var isInit = false
+private var IS_DEBUG = false
+private var IS_LOG = false
+
+fun config(isDebug: Boolean, isLog: Boolean = IS_LOG) {
+    if (isInit) {
+        throw RuntimeException("config is init")
+    }
+    isInit = true
+    IS_DEBUG = isDebug
+    IS_LOG = isLog
+}
+
+data class AppConfig(val isDebug: Boolean, val isLog: Boolean)
+
+fun getAppconfig(): AppConfig = AppConfig(isDebug = IS_DEBUG, isLog = IS_LOG)
