b89b90c953e0fcebb6ea9a758ba32ed471288887
zhaozhe <yehengzhishang@163.com>
2019-12-27 08:15:15 +0800

    update

--- app/src/main/java/com/yu/zz/topbook/deep/TopBookApi.kt
+++ app/src/main/java/com/yu/zz/topbook/deep/TopBookApi.kt
@@ -1,5 +1,6 @@
 package com.yu.zz.topbook.deep
 
+import com.yu.zz.common.getAppConfig
 import com.yu.zz.common.net.FlyNet
 import com.yu.zz.common.net.FlyNetConfig
 import retrofit2.Retrofit
@@ -7,7 +8,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class TopBookApi private constructor() {
-    private val fly: FlyNet = FlyNet(FlyNetConfig(TOPBOOK_URL_BASE, RxJava2CallAdapterFactory.create(), GsonConverterFactory.create()))
+    private val fly: FlyNet = FlyNet(FlyNetConfig(baseUrl = TOPBOOK_URL_BASE, callFactory = RxJava2CallAdapterFactory.create(), converterFactory = GsonConverterFactory.create(), isDebug = getAppConfig().isDebug))
     val retrofit: Retrofit = fly.retrofit
 
     companion object {
