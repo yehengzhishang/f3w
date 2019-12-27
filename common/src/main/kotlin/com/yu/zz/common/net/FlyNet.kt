1a9b1fb5f3a1d06e210fba437e4f58c02d235e76
zhaozhe <yehengzhishang@163.com>
2019-12-27 08:12:52 +0800

    update api

--- common/src/main/kotlin/com/yu/zz/common/net/FlyNet.kt
+++ common/src/main/kotlin/com/yu/zz/common/net/FlyNet.kt
@@ -1,11 +1,12 @@
 package com.yu.zz.common.net
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.CallAdapter
 import retrofit2.Converter
 import retrofit2.Retrofit
 
-class FlyNet constructor(private val config: FlyNetConfig) {
+class FlyNet constructor(config: FlyNetConfig) {
     val retrofit: Retrofit = Retrofit.Builder()
             .client(config.getClient())
             .baseUrl(config.baseUrl)
@@ -14,6 +15,17 @@ class FlyNet constructor(private val config: FlyNetConfig) {
             .build()
 }
 
-open class FlyNetConfig constructor(val baseUrl: String, val callFactory: CallAdapter.Factory, val converterFactory: Converter.Factory) {
-    open fun getClient(): OkHttpClient = OkHttpClient.Builder().build()
+open class FlyNetConfig constructor(val isDebug: Boolean, val baseUrl: String, val callFactory: CallAdapter.Factory, val converterFactory: Converter.Factory) {
+    open fun getClient(): OkHttpClient = getClientBuilder().build()
+    open fun getClientBuilder(): OkHttpClient.Builder = OkHttpClient.Builder()
+            .apply {
+                if (isDebug) {
+                    this.addNetworkInterceptor(getLogger())
+                }
+            }
+
+
+    open fun getLogger(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        this.level = HttpLoggingInterceptor.Level.BASIC
+    }
 }
