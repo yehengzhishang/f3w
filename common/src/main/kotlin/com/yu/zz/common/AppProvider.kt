5c2ccf22b7e332bce578618ee7fa0078f53080af
zhaozhe <yehengzhishang@163.com>
2019-12-28 13:57:36 +0800

    add provider init

---
+++ common/src/main/kotlin/com/yu/zz/common/AppProvider.kt
@@ -0,0 +1,24 @@
+package com.yu.zz.common
+
+import android.app.Application
+import android.util.Log
+
+private var provider: AppProvider? = null
+
+fun appInit(app: Application) {
+    if (provider != null) {
+        Log.e("rain", "app provider is init")
+    }
+    provider = AppProvider(app)
+}
+
+fun getAppProvider(): AppProvider {
+    if (provider == null) {
+        throw RuntimeException("app not init")
+    }
+    return provider!!
+}
+
+fun getProviderApplication(): Application = getAppProvider().app
+
+class AppProvider constructor(val app: Application)
