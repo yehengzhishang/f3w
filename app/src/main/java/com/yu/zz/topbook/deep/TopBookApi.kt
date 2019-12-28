c1b3b58023532a46dd721f9b69d3b2c72088260b
zhaozhe <yehengzhishang@163.com>
2019-12-28 11:10:54 +0800

    change instance factory

--- app/src/main/java/com/yu/zz/topbook/deep/TopBookApi.kt
+++ app/src/main/java/com/yu/zz/topbook/deep/TopBookApi.kt
@@ -3,12 +3,12 @@ package com.yu.zz.topbook.deep
 import com.yu.zz.common.getAppConfig
 import com.yu.zz.common.net.FlyNet
 import com.yu.zz.common.net.FlyNetConfig
+import com.yu.zz.common.net.getFactoryDefaultCall
+import com.yu.zz.common.net.getFactoryDefaultConverter
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 class TopBookApi private constructor() {
-    private val fly: FlyNet = FlyNet(FlyNetConfig(baseUrl = TOPBOOK_URL_BASE, callFactory = RxJava2CallAdapterFactory.create(), converterFactory = GsonConverterFactory.create(), isDebug = getAppConfig().isDebug))
+    private val fly: FlyNet = FlyNet(FlyNetConfig(baseUrl = TOPBOOK_URL_BASE, callFactory = getFactoryDefaultCall(), converterFactory = getFactoryDefaultConverter(), isDebug = getAppConfig().isDebug))
     val retrofit: Retrofit = fly.retrofit
 
     companion object {
