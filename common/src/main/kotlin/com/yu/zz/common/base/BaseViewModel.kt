6428a1f30a4659706e4e5f562e8cfc2d58762d91
zhaozhe <yehengzhishang@163.com>
2020-01-14 08:43:55 +0800

    LiveData provider

--- common/src/main/kotlin/com/yu/zz/common/base/BaseViewModel.kt
+++ common/src/main/kotlin/com/yu/zz/common/base/BaseViewModel.kt
@@ -1,6 +1,68 @@
 package com.yu.zz.common.base
 
 import android.app.Application
+import androidx.core.util.Pair
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 
-open class BaseViewModel(app: Application) : AndroidViewModel(app)
+const val DEFAULT_FIRST = 0
+const val DEFAULT_MESSAGE = ""
+
+open class BaseViewModel(app: Application) : AndroidViewModel(app) {
+    private val mDataToast: ProviderData<String> by lazy {
+        MessageProviderData()
+    }
+
+    private val mDataMessage: PairProviderData<Int, String> by lazy {
+        IntAndMsgProviderData(DEFAULT_FIRST, DEFAULT_MESSAGE)
+    }
+
+    fun getDataToast(): LiveData<String> = mDataToast.data
+    fun getDataMessage(): LiveData<Pair<Int, String>> = mDataMessage.data
+}
+
+interface ProviderData<T> {
+    val data: LiveData<T>
+    fun set(t: T) {
+        val data = data
+        if (data !is MutableLiveData) {
+            return
+        }
+        val dataMu: MutableLiveData<T> = data
+        dataMu.value = t
+    }
+
+    fun post(t: T) {
+        val data = data
+        if (data !is MutableLiveData) {
+            return
+        }
+        val dataMu: MutableLiveData<T> = data
+        dataMu.postValue(t)
+    }
+}
+
+interface PairProviderData<F, S> : ProviderData<Pair<F, S>> {
+    val defaultF: F
+    val defaultS: S
+    fun defaultFirst(s: S) {
+        set(Pair(defaultF, s))
+    }
+
+    fun defaultSecond(f: F) {
+        set(Pair(f, defaultS))
+    }
+
+    fun zipSet(f: F, s: S) {
+        set(Pair(f, s))
+    }
+}
+
+class MessageProviderData(override val data: LiveData<String>) : ProviderData<String> {
+    constructor() : this(MutableLiveData<String>())
+}
+
+class IntAndMsgProviderData(override val data: LiveData<Pair<Int, String>>, override val defaultF: Int, override val defaultS: String) : PairProviderData<Int, String> {
+    constructor(code: Int, message: String) : this(MutableLiveData<Pair<Int, String>>(), code, message)
+}
