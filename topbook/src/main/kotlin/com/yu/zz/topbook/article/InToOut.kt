25fc9b5feef9232d8417cbcc765343f82cd5aeef
zhaozhe <yehengzhishang@163.com>
2020-04-18 11:30:54 +0800

    article 跳转

---
+++ topbook/src/main/kotlin/com/yu/zz/topbook/article/InToOut.kt
@@ -0,0 +1,13 @@
+package com.yu.zz.topbook.article
+
+import android.app.Activity
+import com.google.android.material.snackbar.Snackbar
+import com.yu.zz.topbook.employ.ArticleTopBookBean
+
+fun click(activity: Activity): (bean: ArticleTopBookBean, position: Int) -> Unit {
+    return { _, _ ->
+        activity.window.decorView.let { view ->
+            Snackbar.make(view, "文章详情页面正在生成中", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
