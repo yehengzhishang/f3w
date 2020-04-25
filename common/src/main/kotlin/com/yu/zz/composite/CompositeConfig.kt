eaaba7adb937215eaec2a542c0d24e8d11e151c4
zhaozhe <yehengzhishang@163.com>
2020-04-25 10:44:30 +0800

    delete

--- common/src/main/kotlin/com/yu/zz/composite/CompositeConfig.kt
+++ common/src/main/kotlin/com/yu/zz/composite/CompositeConfig.kt
@@ -1,11 +1,9 @@
 package com.yu.zz.composite
 
 import android.app.Application
-import com.yu.zz.common.arrange.configToast
 import com.yu.zz.common.base.appInit
 
 fun compositeInit(app: Application) {
-    configToast(app)
     appInit(app)
     app.registerActivityLifecycleCallbacks(CompositeLifecycle())
 }
