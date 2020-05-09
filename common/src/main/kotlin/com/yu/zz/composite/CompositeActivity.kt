f7eaf0f9fa5aa1e36b9c42f60f8c79b1de8fcbda
zhaozhe <yehengzhishang@163.com>
2020-05-10 06:04:29 +0800

    增加方法

--- common/src/main/kotlin/com/yu/zz/composite/CompositeActivity.kt
+++ common/src/main/kotlin/com/yu/zz/composite/CompositeActivity.kt
@@ -1,7 +1,10 @@
 package com.yu.zz.composite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
 import com.yu.zz.common.base.BaseActivity
+import com.yu.zz.bypass.createViewModel as createVMByFactory
 import com.yu.zz.common.arrange.createViewModel as createVM
 
 abstract class CompositeActivity : BaseActivity() {
@@ -11,5 +14,13 @@ abstract class CompositeActivity : BaseActivity() {
 
     protected open fun <T : ViewModel> createViewModel(clazz: Class<T>): T {
         return createVM(clazz)
+    }
+
+    protected inline fun <reified T : ViewModel> createViewModel(viewModelStore: ViewModelStore, factory: ViewModelProvider.Factory): T {
+        return createVMByFactory(viewModelStore, factory, T::class.java)
+    }
+
+    protected open fun <T : ViewModel> createViewModel(viewModelStore: ViewModelStore, factory: ViewModelProvider.Factory, clazz: Class<T>): T {
+        return createVMByFactory(viewModelStore, factory, clazz)
     }
 }
--- common/src/main/kotlin/com/yu/zz/composite/CompositeFragment.kt
+++ common/src/main/kotlin/com/yu/zz/composite/CompositeFragment.kt
@@ -2,7 +2,10 @@ package com.yu.zz.composite
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
 import com.yu.zz.bypass.createViewModelActivity
+import com.yu.zz.bypass.createViewModel as createVMByFactory
 import com.yu.zz.common.arrange.createViewModel as creteVM
 
 open class CompositeFragment : Fragment() {
@@ -12,6 +15,10 @@ open class CompositeFragment : Fragment() {
 
     protected inline fun <reified T : ViewModel> createViewModel(): T {
         return creteVM()
+    }
+
+    protected open fun <T : ViewModel> createViewModel(viewModelStore: ViewModelStore, factory: ViewModelProvider.Factory, clazz: Class<T>): T {
+        return createVMByFactory(viewModelStore, factory, clazz)
     }
 
     protected open fun <T : ViewModel> createViewModelByActivity(clazz: Class<T>): T {
