d1ae70e6279a1166a00505159da4a0b288ef9399
zhaozhe <yehengzhishang@163.com>
2020-05-19 15:31:49 +0800

    url viewpoint

--- topbook/src/main/kotlin/com/yu/zz/topbook/employ/TopBookUrl.kt
+++ topbook/src/main/kotlin/com/yu/zz/topbook/employ/TopBookUrl.kt
@@ -8,4 +8,6 @@ const val TOPBOOK_URL_PATH_PREFIX = "webapi/content/article/{$TOPBOOK_PATH_CATEG
 const val TOPBOOK_URL_PAGE = "webapi/content/category/page"
 
 const val TOPBOOK_URL_TOPIC_LIST = "webapi/community/topic/page"
+
+const val TOPBOOK_URL_TOPIC_VIEWPOINTS = "webapi/community/viewpoint/page"
 //fun getTopBookUrlPathString(pos: String): String = String.format(TOPBOOK_URL_PATH_PREFIX, pos)
