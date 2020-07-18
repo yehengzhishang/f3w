e81ca673b5eeecdcd6890967af39d4252ea0b477
yehengzhishang <yehengzhishang@163.com>
2020-07-19 07:30:03 +0800

    bridge 扩充

--- topbook/src/main/kotlin/com/yu/zz/topbook/employ/TopBookViewHolder.kt
+++ topbook/src/main/kotlin/com/yu/zz/topbook/employ/TopBookViewHolder.kt
@@ -6,8 +6,21 @@ import android.view.ViewGroup
 import com.yu.zz.common.base.BaseViewHolder
 
 abstract class TopBookViewHolder<Bean> constructor(itemView: View) : BaseViewHolder<Bean>(itemView) {
-    val mapBridge = HashMap<String, Any>()
+    private val bridgeMap = mutableMapOf<String, Any>()
     var click: ((Bean, Int) -> Unit)? = null
 
     constructor(parent: ViewGroup, layoutId: Int) : this(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
+
+    @Suppress("UNCHECKED_CAST")
+    protected fun <T> bridgePut(key: String): T {
+        return bridgeMap[key] as T
+    }
+
+    protected fun bridgeHas(key: String): Boolean {
+        return bridgeMap.containsKey(key)
+    }
+
+    fun bridgePut(key: String, value: Any) {
+        bridgeMap[key] = value
+    }
 }
