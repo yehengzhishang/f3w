aef2ef8458d195ddb84debec990a95f3397e4a0d
yehengzhishang <yehengzhishang@163.com>
2020-08-14 12:27:02 +0800

    fragment view model

---
+++ app/src/main/java/com/yu/zz/ViewModelFragmentActivity.kt
@@ -0,0 +1,61 @@
+package com.yu.zz
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.yu.zz.fwww.R
+import com.yu.zz.topbook.employ.TopBookViewModel
+import kotlinx.android.synthetic.main.activity_fragment_view_model.*
+
+class ViewModelFragmentActivity : AppCompatActivity() {
+    private val fragment = ViewModelFragment();
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_view_model)
+        addFragment()
+        btn_debug.setOnClickListener {
+            log()
+        }
+        btn_remove.setOnClickListener { removeFragment() }
+    }
+
+    private fun addFragment() {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fl, fragment, fragment.toString())
+                .show(fragment).commitAllowingStateLoss()
+    }
+
+    private fun log(){
+        Log.e("rain", "debug")
+    }
+
+    private fun removeFragment() {
+        supportFragmentManager.beginTransaction().remove(fragment).commitAllowingStateLoss()
+    }
+
+}
+
+class ViewModelFragment : Fragment() {
+    private val view: TopBookViewModel by lazy {
+        ViewModelProvider(this)[TopBookViewModel::class.java]
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        view.getApplication<App>().toString()
+        return inflater.inflate(R.layout.activity_main, container, false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e("rain", "destroy")
+    }
+
+
+}
