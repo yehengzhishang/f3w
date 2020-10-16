e6b5dd9be83d26e120b9cf728ede13cf79ea6cf4
yehengzhishang <yehengzhishang@163.com>
2020-10-16 22:40:25 +0800

    bean

--- up/src/main/java/com/yu/zz/up/TodoEntity.kt
+++ up/src/main/java/com/yu/zz/up/TodoEntity.kt
@@ -4,10 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+const val DEFAULT_GROUP = "task"
+
+@Entity(tableName = "todo_table")
 class TodoEntity {
     @PrimaryKey(autoGenerate = true)
-    var id: Long = 0
+    @ColumnInfo(name = "todo_id")
+    var todoId: Long = 0
 
     // 直属父id
     @ColumnInfo(name = "parent_id")
@@ -50,10 +53,34 @@ class TodoEntity {
     // 标签
     @ColumnInfo(name = "tag")
     var tag: String? = null
+
+    //分组id
+    @ColumnInfo(name = "list_id")
+    var listId: String = DEFAULT_GROUP
 }
 
 @Entity(tableName = "color_table")
-class ColorTag {
+class ColorEntity {
     @PrimaryKey
+    @ColumnInfo(name = "color")
     var color: String? = null
+}
+
+@Entity(tableName = "group_table")
+class GroupEntity {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "group_id")
+    var groupId: Long? = null
+
+    @ColumnInfo(name = "group_name")
+    var groupName: String? = null
+}
+
+@Entity(tableName = "list_table")
+class ListEntity {
+    @ColumnInfo(name = "list_id")
+    var listId: Long? = null
+
+    @ColumnInfo(name = "list_name")
+    var listName: String? = null
 }
