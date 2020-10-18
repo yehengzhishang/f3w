90de2b6b113b30ed392a050f53ac7fbd57b0ae33
yehengzhishang <yehengzhishang@163.com>
2020-10-18 09:26:09 +0800

    user_测试

--- up/src/main/java/com/yu/zz/up/user/User.kt
+++ up/src/main/java/com/yu/zz/up/user/User.kt
@@ -1,18 +1,23 @@
 package com.yu.zz.up.user
 
+import android.util.Log
 import androidx.room.*
-import com.yu.zz.bypass.goToThreadMain
 import com.yu.zz.common.base.BaseActivity
 import com.yu.zz.up.R
 import io.reactivex.Flowable
 import io.reactivex.FlowableSubscriber
+import io.reactivex.functions.Consumer
+import kotlinx.android.synthetic.main.user_zz_activity_zz_test.*
+import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
 @Entity(tableName = "user_table")
 class UserEntity {
     @PrimaryKey(autoGenerate = true)
-    var id: Long? = null
+    @ColumnInfo(name = "user_id")
+    var id: Long = 0
 
+    @ColumnInfo(name = "user_name")
     var name: String? = null
 }
 
@@ -23,6 +28,9 @@ interface UserDao {
 
     @Query("select * from user_table")
     fun queryAll(): Flowable<List<UserEntity>>
+
+    @Query("select * from user_table")
+    fun all(): List<UserEntity>
 }
 
 @Database(entities = [UserEntity::class], version = 1)
@@ -45,27 +53,109 @@ class UserActivity : BaseActivity() {
     }
 
     override fun createSecondUi() {
-
+        btn.setOnClickListener {
+            val name = tie_name.text.toString()
+            create(UserEntity().apply {
+                this.name = name
+            })
+        }
     }
 
     override fun createThirdData() {
-        dao.queryAll()
-                .goToThreadMain()
-                .subscribe(object : FlowableSubscriber<List<UserEntity>> {
+        Flowable.just("abc")
+                .subscribe(object : Subscriber<String> {
                     override fun onComplete() {
-                    }
-
-                    override fun onSubscribe(s: Subscription) {
 
                     }
 
-                    override fun onNext(t: List<UserEntity>) {
+                    override fun onSubscribe(s: Subscription?) {
 
+                    }
+
+                    override fun onNext(t: String?) {
+                        Log.e("rain", t)
                     }
 
                     override fun onError(t: Throwable?) {
 
                     }
+
                 })
+        dao.queryAll().subscribe(AllUserFlowableSubscriber())
+        dao.queryAll().subscribe(AllUserConsumer())
+        dao.queryAll().subscribe(AllUserSubscriber())
+        Flowable.just(mutableListOf<UserEntity>()).subscribe(AllUserSubscriber())
+    }
+
+    private fun create(user: UserEntity) {
+        Thread {
+            val id = dao.insertOne(user)
+            if (id == null) {
+                Log.v("rain", "null")
+            } else {
+                Log.v("rain", id.toString())
+            }
+            val list = dao.all()
+
+            Log.v("rain", "u start = = = ")
+            Log.v("rain", "id  | name")
+            for (u in list) {
+                Log.v("rain", "${u.id} | ${u.name}")
+            }
+            Log.v("rain", "end = = =")
+
+        }.start()
+    }
+
+    inner class AllUserSubscriber : Subscriber<List<UserEntity>> {
+        override fun onComplete() {
+            log("onComplete")
+        }
+
+        override fun onSubscribe(s: Subscription?) {
+            log("onSubscribe")
+            s?.request(1000)
+        }
+
+        override fun onNext(t: List<UserEntity>?) {
+            log("onNext")
+        }
+
+        override fun onError(t: Throwable?) {
+            log("onError")
+        }
+
+        private fun log(message: String) {
+            Log.v("rain", javaClass.simpleName + " || $message")
+        }
+    }
+
+    inner class AllUserConsumer : Consumer<List<UserEntity>> {
+        override fun accept(t: List<UserEntity>?) {
+            Log.v("rain", javaClass.simpleName + " accept")
+        }
+    }
+
+    inner class AllUserFlowableSubscriber : FlowableSubscriber<List<UserEntity>> {
+        override fun onComplete() {
+            log("onComplete")
+        }
+
+        override fun onSubscribe(s: Subscription) {
+            log("onSubscribe")
+
+        }
+
+        override fun onNext(t: List<UserEntity>?) {
+            log("onNext")
+        }
+
+        override fun onError(t: Throwable?) {
+            log("onError")
+        }
+
+        private fun log(message: String) {
+            Log.v("rain", javaClass.simpleName + " || $message")
+        }
     }
 }
