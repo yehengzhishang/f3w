738855ac1580d221764a1f3b436c4f0a817335db
yehengzhishang <yehengzhishang@163.com>
2020-10-18 10:42:31 +0800

    add gradation adapter

---
+++ up/src/main/java/com/yu/zz/up/GradationAdapter.kt
@@ -0,0 +1,16 @@
+package com.yu.zz.up
+
+import android.view.View
+import com.yu.zz.common.base.BaseViewHolder
+
+class GradationAdapter {
+
+}
+
+class GradationViewHolder(view: View) : BaseViewHolder<GradationInfo>(view) {
+
+    override fun bind(bean: GradationInfo, position: Int) {
+        super.bind(bean, position)
+
+    }
+}
