5c7bfb0cce8ab956afe8ac9d2fab67744e5214da
yehengzhishang <yehengzhishang@163.com>
2020-10-18 10:42:08 +0800

    update todo reop

--- up/src/main/java/com/yu/zz/up/TodoRepository.kt
+++ up/src/main/java/com/yu/zz/up/TodoRepository.kt
@@ -1,5 +1,9 @@
 package com.yu.zz.up
 
+import com.yu.zz.bypass.goToThreadIO
+import io.reactivex.Flowable
+import io.reactivex.functions.BiFunction
+
 class TodoRepository {
     fun queryOne(todoId: String): TodoEntity {
         return TodoEntity()
@@ -10,4 +14,45 @@ class TodoRepository {
 class TodoCache constructor(val map: Map<String, TodoEntity>) : Map<String, TodoEntity> by map {
 
 
+}
+
+class GradationRepository(private val daoList: ListDao, private val daoGroup: GroupDao) {
+    fun loadAll(): Flowable<List<GradationInfo>> {
+        return Flowable.zip(
+                daoGroup.queryPersistentAll().goToThreadIO()
+                , daoList.queryPersistentNoGroup().goToThreadIO()
+                , GradationInfoZip()
+        )
+    }
+}
+
+class GradationInfoZip : BiFunction<List<GroupEntity>, List<ListEntity>, List<GradationInfo>> {
+    override fun apply(lists: List<GroupEntity>, groups: List<ListEntity>): List<GradationInfo> {
+        return mutableListOf<GradationInfo>().apply {
+            for (list in lists) {
+                add(GradationInfo(list))
+            }
+            for (group in groups) {
+                add(GradationInfo(group))
+            }
+        }
+    }
+}
+
+class GradationInfo(private val entity: Any) {
+    fun isList(): Boolean {
+        return entity is ListEntity
+    }
+
+    fun isGroup(): Boolean {
+        return entity is GroupEntity
+    }
+
+    fun getList(): ListEntity {
+        return entity as ListEntity
+    }
+
+    fun getGroup(): GroupEntity {
+        return entity as GroupEntity
+    }
 }
