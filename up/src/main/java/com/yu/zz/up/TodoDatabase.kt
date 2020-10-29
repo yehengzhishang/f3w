c8d9579fc92f310c3fc075b3ba68f38811ed455a
yehengzhishang <yehengzhishang@163.com>
2020-10-29 08:37:54 +0800

    db update

--- up/src/main/java/com/yu/zz/up/TodoDatabase.kt
+++ up/src/main/java/com/yu/zz/up/TodoDatabase.kt
@@ -1,9 +1,17 @@
 package com.yu.zz.up
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [TodoEntity::class, ListEntity::class, GroupEntity::class], version = 1)
 abstract class TodoDatabase : RoomDatabase() {
     abstract fun getDaoTodo(): TodoDao
+    abstract fun getDaoList(): ListDao
+    abstract fun getDaoGroup(): GroupDao
+}
+
+fun todoDataBase(context: Context): TodoDatabase {
+    return Room.databaseBuilder(context, TodoDatabase::class.java, "todo_db").build()
 }
