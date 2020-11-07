ba9abf19b04b5db87896e44c8ac35d9ca0d9f2a4
yehengzhishang <yehengzhishang@163.com>
2020-11-07 16:32:47 +0800

    base

--- up/src/main/java/com/yu/zz/up/UpActivity.kt
+++ up/src/main/java/com/yu/zz/up/UpActivity.kt
@@ -1,6 +1,10 @@
 package com.yu.zz.up
 
 import android.app.Application
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yu.zz.common.base.BaseViewHolder
 import com.yu.zz.composite.CompositeActivity
 import com.yu.zz.composite.CompositeViewModel
 
@@ -9,3 +13,8 @@ abstract class UpActivity : CompositeActivity() {
 }
 
 open class UpViewModel(app: Application) : CompositeViewModel(app)
+
+
+abstract class UpViewHolder<T> constructor(view: View) : BaseViewHolder<T>(view) {
+    constructor(parent: ViewGroup, layoutId: Int) : this(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
+}
